d8e54988419ae26a570abe0a2b15a56895a414f7
badiwidya <adiwidya.berton13@gmail.com>
2025-06-27 01:53:37 +0700

    Define complete PlacesManager functionality

--- Assets/Scripts/PlacesManager.cs
+++ Assets/Scripts/PlacesManager.cs
@@ -1,16 +1,154 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class NarrationPage
+{
+    [TextArea(5, 10)] public string narrationText;
+}
+
+[System.Serializable]
+public class PlaceData
+{
+    public string placeName;
+    public GameObject sphereObject;
+    public GameObject narrationPanel;
+    [CanBeNull] public GameObject quizPanel;
+
+    public NarrationPage[] narrationPages;
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PlaceData[] allPlaces;
+
+    public TMP_Text narrationDisplayText;
+
+    public Button narrationPreviousButton;
+
+    public Button narrationNextButton;
+
+    public Button narrationContinueButton;
+
+    public TMP_Text narrationContinueButtonText;
+
+    private int _currentPlaceIndex = 0;
+    private int _currentNarrationPageIndex = 0;
+
     void Start()
     {
-        
+        foreach (var place in allPlaces)
+        {
+            if (place.sphereObject != null) place.sphereObject.SetActive(false);
+            if (place.narrationPanel != null) place.narrationPanel.SetActive(false);
+            if (place.quizPanel != null) place.quizPanel.SetActive(false);
+        }
+
+        ShowPlace(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowPlace(int index)
     {
-        
+        if (index < 0 || index >= allPlaces.Length) return;
+
+        HideCurrentPlace();
+
+        _currentPlaceIndex = index;
+        _currentNarrationPageIndex = 0;
+
+        var currentPlace = allPlaces[_currentPlaceIndex];
+        if (currentPlace.sphereObject != null)
+        {
+            currentPlace.sphereObject.SetActive(true);
+        }
+
+        ShowNarrationForCurrentPlace();
+    }
+
+    public void ShowNextPlace()
+    {
+        var nextIndex = _currentPlaceIndex + 1;
+        if (nextIndex >= allPlaces.Length) nextIndex = 0;
+        ShowPlace(nextIndex);
+    }
+
+    public void ShowNarrationForCurrentPlace()
+    {
+        var currentPlace = allPlaces[_currentPlaceIndex];
+
+        if (currentPlace.quizPanel != null) currentPlace.quizPanel.SetActive(false);
+
+        if (currentPlace.narrationPanel == null) return;
+
+        currentPlace.narrationPanel.SetActive(true);
+        UpdateNarrationContent();
+    }
+
+    public void ShowQuizForCurrentPlace()
+    {
+        var currentPlace = allPlaces[_currentPlaceIndex];
+        if (currentPlace.quizPanel == null)
+        {
+            ShowNextPlace();
+            return;
+        }
+
+        if (currentPlace.narrationPanel != null) currentPlace.narrationPanel.SetActive(false);
+
+        currentPlace.quizPanel.SetActive(true);
+    }
+
+    public void ShowNextNarrationPage()
+    {
+        var currentPlace = allPlaces[_currentPlaceIndex];
+        if (_currentNarrationPageIndex >= currentPlace.narrationPages.Length - 1) return;
+        _currentNarrationPageIndex++;
+        UpdateNarrationContent();
+    }
+
+    public void ShowPreviousNarrationPage()
+    {
+        if (_currentNarrationPageIndex <= 0) return;
+        _currentNarrationPageIndex--;
+        UpdateNarrationContent();
+    }
+
+    private void UpdateNarrationContent()
+    {
+        var currentPlace = allPlaces[_currentPlaceIndex];
+
+        narrationDisplayText.text = currentPlace.narrationPages[_currentNarrationPageIndex].narrationText;
+
+        narrationPreviousButton.interactable = (_currentNarrationPageIndex > 0);
+        narrationNextButton.interactable = (_currentNarrationPageIndex < currentPlace.narrationPages.Length - 1);
+
+        narrationContinueButton.gameObject.SetActive(_currentNarrationPageIndex ==
+                                                     currentPlace.narrationPages.Length - 1);
+
+        narrationContinueButtonText.text = currentPlace.quizPanel != null ? "Lanjut ke Kuis" : "Selanjutnya";
+    }
+
+    public void OnContinueButtonPressed()
+    {
+        var currentPlace = allPlaces[_currentPlaceIndex];
+        if (currentPlace.quizPanel != null)
+        {
+            ShowQuizForCurrentPlace();
+        }
+        else
+        {
+            ShowNextPlace();
+        }
+    }
+
+    private void HideCurrentPlace()
+    {
+        if (allPlaces.Length == 0) return;
+        var currentPlace = allPlaces[_currentPlaceIndex];
+        if (currentPlace.sphereObject != null) currentPlace.sphereObject.SetActive(false);
+        if (currentPlace.narrationPanel != null) currentPlace.narrationPanel.SetActive(false);
+        if (currentPlace.quizPanel != null) currentPlace.quizPanel.SetActive(false);
     }
 }
