cddd624e7a6da9d1363f5fd67a1b8a0c1993feaf
badiwidya <adiwidya.berton13@gmail.com>
2025-06-27 10:18:52 +0700

    Add height offset to UIPositioner.cs to set the y position

--- Assets/Scripts/UIPositioner.cs
+++ Assets/Scripts/UIPositioner.cs
@@ -5,13 +5,16 @@ public class UIPositioner : MonoBehaviour
     public Transform mainCamera;
     
     public float spawnDistance = 1.0f;
+    public float heightOffset = 0.0f;
 
     public void PositionObjectInFrontOfPlayer(GameObject objectToPosition)
     {
         if (mainCamera == null || objectToPosition == null) return;
 
-        var targetPosition = mainCamera.position + (mainCamera.forward * spawnDistance);
+        var forwardPosition = mainCamera.position + (mainCamera.forward * spawnDistance);
 
+        var targetPosition = new Vector3(forwardPosition.x, mainCamera.position.y + heightOffset, forwardPosition.z);
+        
         var cameraAngles = mainCamera.rotation.eulerAngles;
         var targetRotation = Quaternion.Euler(0, cameraAngles.y, 0);
 
